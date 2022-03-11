e1d6501236fc3f0dbd1bafe6f28f80c1502a4dc9
ArpimientaU <arpimienta@uniguajira.edu.co>
2022-03-10 20:00:02 -0500

    aplicacion de los diccionarios

---
+++ actividad2/programa3.py
@@ -0,0 +1,17 @@
+from api.library import *
+
+def main():
+
+    persona = {
+        "datospersonales":{
+            "nombre": "jaider",
+            "apellidos": "quintero mendoza",
+            "edad": 45
+        },
+
+    
+
+    }
+
+if __name__ == "__main__":
+    main()
