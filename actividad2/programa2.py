ca1e558239f22a86e28b6f70e35b753165cfd1bd
Dubian <dsamueLuriana@uniguajira.edu.co>
2022-03-10 21:39:18 -0500

    mostrar, agregar, eliminar y remover elementos de una lista

--- actividad2/programa2.py
+++ actividad2/programa2.py
@@ -19,3 +19,28 @@ def listas():
         j+=1
     
     listaConElementos[1]= listaConElementos[1] + 200000
+    
+    print(listaConElementos[5][3])
+    print(listaConElementos[-1][3])
+    print(listaConElementos[0:3]) # Muestra la posicion de la 0 hasta la 3, sin entrar la 3
+    print(listaConElementos[1:3:5])
+    print(listaConElementos[1:6:2])
+    print(listaConElementos[0:6:2])
+
+    #listaConElementos.append("Sede Riohacha" "Miguel Soto") # Agrega elemento al final de la lista
+
+    listaConElementos.insert(2, ["Sede Riohacha" "Miguel Soto"]) # Agrega elementos en la posicion X
+    print(listaConElementos)
+
+    del listaConElementos[2] # Elimina elementos de la posicion X
+    print(listaConElementos)
+
+    listaConElementos.remove("Docente") # Remueve el elemento de acuerdo al contenido
+    print(listaConElementos)
+
+
+def main():
+        listas()
+
+if __name__ == "__main__":
+    main()
