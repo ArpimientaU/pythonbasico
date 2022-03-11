409abf807b8f3deb42d5369142ecb98ebf95914c
Dubian <dsamueLuriana@uniguajira.edu.co>
2022-03-10 21:30:17 -0500

    ingresar valores en pantalla

--- actividad2/programa1.py
+++ actividad2/programa1.py
@@ -7,5 +7,17 @@ def main():
     SUB_TRANSP = 80000
     BONO = 50000
 
+    nombre = input("Digite el nombre: ")
+    salario = int(input("Digite el salario mensual: "))
+    diastrabajados = int(input("digite los dias trabajados: "))
+    sueldopagar = calcularsueldo(salario, diastrabajados)
+
+    if salario < (SALARIO_MIN * 2):
+        sueldopagar = sueldopagar + SUB_ALIM + SUB_TRANSP
+    else:
+        sueldopagar = sueldopagar + BONO
+
+    print(f"mi nombre es: {nombre} y mi sueldo es {sueldopagar:.0f}")
+
 if __name__ == "__main__":
     main()
