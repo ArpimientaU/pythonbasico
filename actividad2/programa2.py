664f51766b74c1911992aed3c836422239210ed4
ArpimientaU <arpimienta@uniguajira.edu.co>
2022-03-10 19:32:20 -0500

    lista con elementos y utilizando el ciclo for

---
+++ actividad2/programa2.py
@@ -0,0 +1,10 @@
+
+
+def listas():
+    lista1 = []
+    lista2 = list()
+
+    listaConElementos = [30, 2000000, "Andres Pimienta", "Docente", True, ["Magister", "Doctorado", "Especializacion", 20]]
+
+    for i in range(len(listaConElementos)):
+        print(listaConElementos[i])
